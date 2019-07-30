ffeb0d570a5ab1bad1692f3bce5cab64610d7751
Hui Zhang <buaa.zhanghui@gmail.com>
2019-07-29 21:15:43 -0400

    Update change

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 # If a command fails then the deploy stops
 set -e
 
-git config --global user.name "ericzhng"
+# git config --global user.name "ericzhng"
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+printf "\033[0;32m Deploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
@@ -22,10 +22,10 @@ if [ -n "$*" ]; then
 	msg="$*"
 fi
 
-git stage
+#git stage
 git commit -m "$msg"
 
 # Push source and build repos.
-git push
+git push origin master
 
-# git submodule add -b master https://github.com/ericzhng/ericzhng.github.io.git public1
+# git submodule add -b master https://github.com/ericzhng/ericzhng.github.io.git public
