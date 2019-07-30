840ada6b80c9a7a3361e8a643d18f9b096ffc64d
ericzhng <ericzhng@DESKTOP-LJK5KPU.localdomain>
2019-07-29 21:03:41 -0400

    rebuilding site Mon Jul 29 21:03:40 DST 2019

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,4 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
-
-git git submodule add -b master https://github.com/ericzhng/ericzhng.github.io.git public
+git push https://github.com/ericzhng/ericzhng.github.io.git master
